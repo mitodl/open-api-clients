72429274a2cafe62bc621950b1d52e5dc466f38d
Nathan Levesque <rhysyngsun@gmail.com>
2024-02-21 13:58:38 -0500

    Fix nix setup and yarnrc

--- flake.nix
+++ flake.nix
@@ -3,48 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+
+    devenv.url = "github:cachix/devenv";
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
-
+        inputs.devenv.flakeModule
       ];
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: let
-        
-        inherit (inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv;
-        poetryEnv = mkPoetryEnv {
-          projectDir = ./.;
-        };
-      in {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ poetryEnv ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+      perSystem = { pkgs, ... }: {
+        devenv.shells.default = {
           packages = with pkgs; [
             detect-secrets
             poetry
-            nodejs
-            yarn
           ];
-        };
-      };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
 
+          languages.javascript = {
+            enable = true;
+            corepack.enable = true;
+          };
+        };
       };
     };
 }
