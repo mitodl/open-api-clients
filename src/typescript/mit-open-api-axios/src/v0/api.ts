7456636902975dc473a84bba30ee2d67cf2d42cb
MIT Open Learning Engineering <odl-devops@mit.edu>
2024-07-10 18:53:53 +0000

    Generated clients from rev: 172475e0974ce678a2642a720a021df769fa259a

--- src/typescript/mit-open-api-axios/src/v0/api.ts
+++ src/typescript/mit-open-api-axios/src/v0/api.ts
@@ -1995,18 +1995,6 @@ export interface ProgramCertificate {
      * @type {string}
      * @memberof ProgramCertificate
      */
-    'program_letter_generate_url': string;
-    /**
-     * 
-     * @type {string}
-     * @memberof ProgramCertificate
-     */
-    'program_letter_share_url': string;
-    /**
-     * 
-     * @type {string}
-     * @memberof ProgramCertificate
-     */
     'program_title': string;
     /**
      * 
--- src/typescript/mit-open-api-axios/src/v1/api.ts
+++ src/typescript/mit-open-api-axios/src/v1/api.ts
@@ -4119,18 +4119,6 @@ export interface ProgramCertificate {
      * @type {string}
      * @memberof ProgramCertificate
      */
-    'program_letter_generate_url': string;
-    /**
-     * 
-     * @type {string}
-     * @memberof ProgramCertificate
-     */
-    'program_letter_share_url': string;
-    /**
-     * 
-     * @type {string}
-     * @memberof ProgramCertificate
-     */
     'program_title': string;
     /**
      * 
