8cd2fd4e3d6bf13207292f1fa58319421d0700a6
MIT Open Learning Engineering <odl-devops@mit.edu>
2024-08-21 19:03:02 +0000

    Generated clients from rev: f825e55b011bfb968a4f213844f5c884d0ee13a1

--- src/typescript/mit-open-api-axios/src/v0/api.ts
+++ src/typescript/mit-open-api-axios/src/v0/api.ts
@@ -738,6 +738,12 @@ export interface FeedEventDetail {
      * @memberof FeedEventDetail
      */
     'event_datetime': string;
+    /**
+     * 
+     * @type {string}
+     * @memberof FeedEventDetail
+     */
+    'event_end_datetime'?: string | null;
 }
 /**
  * Serializer for FeedImage
