3230ed24370b5d0e7337c1520eaa61b80599d948
Tobias Macey <tmacey@boundlessnotions.com>
2025-04-16 16:10:39 -0400

    fix: Properly handle targeting multiple YAML files
    The globbing isn't expanding in the Concourse pipeline. This uses a more robust (albeit
    complex) means of targeting the files so that they can be at any level of nesting and
    under any target path.
    
    [skip ci]

--- scripts/generate-inner.sh
+++ scripts/generate-inner.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #
-for cfile in mit-open-api-clients/config/*.yaml; do
-	echo "Generating: $cfile"
-	/usr/local/bin/docker-entrypoint.sh generate -c "$cfile"
+script_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+script_parent_dir=$(dirname "$script_dir")
+find ${script_parent_dir} -regex ".*config/.*.yaml" -print0 | while IFS= read -r -d $'\0' cfile; do
+    echo "Generating: $cfile"
+    /usr/local/bin/docker-entrypoint.sh generate -c "$cfile"
 done
