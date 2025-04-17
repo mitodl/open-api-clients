53240f419ac7a3b6747a073de5805449fd267c91
Tobias Macey <tmacey@boundlessnotions.com>
2025-04-17 16:31:02 -0400

    fix: Resolve directory name mismatches

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,13 +5,16 @@
 set -eo pipefail
 shopt -u nullglob
 
+script_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+script_parent_dir=$(dirname "$script_dir")
+
 if [ -z "$(which docker)" ]; then
 	echo "Error: Docker must be available in order to run this script"
 	exit 1
 fi
 
 OPEN_CLONE_DIR=$(mktemp -d)
-OPEN_REPO="https://github.com/mitodl/mit-open.git"
+OPEN_REPO="https://github.com/mitodl/mit-learn.git"
 
 GENERATOR_VERSION="$(cat OPENAPI-GENERATOR-VERSION)"
 GENERATOR_IMAGE=openapitools/openapi-generator-cli:${GENERATOR_VERSION}
@@ -24,11 +27,11 @@ git checkout release
 popd
 
 docker run --rm \
-	-v "${PWD}:/tmp/mit-open-api-clients" \
-	-v "${OPEN_CLONE_DIR}:/tmp/mit-open" \
+	-v "${PWD}:${script_parent_dir}" \
+	-v "${OPEN_CLONE_DIR}:/tmp/mit-learn" \
 	-w /tmp \
 	$GENERATOR_IMAGE \
-	./mit-open-api-clients/scripts/generate-inner.sh
+	.${script_dir}/generate-inner.sh
 
 # set permissions to host permissions so that we can modify files
 docker run --rm \
